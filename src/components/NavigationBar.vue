ab184174a02275f1c7d84e8e58d27c4ea8efedc6
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-02-14 22:42:48 +0100

    [ADD] Color changing current page navbar

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,21 +1,38 @@
 <template>
   <aside>
-    <ul class="navbar flex justify-center gap-5 text-2xl mt-10 text-slate-600 ">
-        <li><router-link to="/">Accueil</router-link></li>
-        <li><router-link to="/map">Cartographie</router-link></li>
-        <li><router-link to="/documentation">Documentation</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
-        <li><router-link to="/flyer">Flyer du site</router-link></li>
-
+    <ul class="navbar flex justify-center gap-5 text-2xl mt-10 ">
+      <li :class="{ 'text-blue-500 underline': currentPage === '/' }">
+        <router-link to="/">Accueil</router-link>
+      </li>
+      <li :class="{ 'text-blue-500 underline': currentPage === '/map' }">
+        <router-link to="/map">Cartographie</router-link>
+      </li>
+      <li :class="{ 'text-blue-500 underline': currentPage === '/documentation' }">
+        <router-link to="/documentation">Documentation</router-link>
+      </li>
+      <li :class="{ 'text-blue-500 underline': currentPage === '/contact' }">
+        <router-link to="/contact">Contact</router-link>
+      </li>
+      <li :class="{ 'text-blue-500 underline': currentPage === '/flyer' }">
+        <router-link to="/flyer">Flyer du site</router-link>
+      </li>
     </ul>
   </aside>
 </template>
 
-<script setup>
-    
+<script>
+export default {
+  data() {
+    return {
+      currentPage: '' // Initialisez currentPage avec le chemin de la page actuelle
+    };
+  },
+  watch: {
+    $route(to) {
+      this.currentPage = to.path; // Met à jour currentPage avec le chemin de la route actuelle
+    }
+  }
+};
 </script>
 
-<style lang="tailwindcss" scoped>
-
-</style>
-
+<style lang="tailwindcss" scoped></style>
