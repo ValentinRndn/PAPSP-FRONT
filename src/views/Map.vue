fb7ab1e48843441f3a435f41b2c0394c8c40cea0
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-17 23:39:03 +0200

    [ADD] Leaflet cartographie

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -3,7 +3,7 @@
         <div class="w-[50vw] flex justify-center items-center">
             <div class="flex flex-col w-[400px] h-[200px] justify-center font-cgothic   ">
                 <router-link to="/cartographie/Particulier" class="bg-purple-fonce text-white px-4 py-6 w-full rounded-md mt-6 text-md font-bold text-center">JE CHERCHE UN SERVICE</router-link>
-                <router-link to="/cartographie/Professionel" class="bg-purple text-white px-4 py-6 w-full rounded-md mt-6 text-md font-bold text-center">JE SQUIS PROFESSIONEL</router-link>
+                <router-link to="/cartographie/Professionel" class="bg-purple text-white px-4 py-6 w-full rounded-md mt-6 text-md font-bold text-center">JE SUIS PROFESSIONEL</router-link>
 
 
             </div>
@@ -13,8 +13,3 @@
     </div>
 </template>
 
-<script>
-export default {
-
-};
-</script>
--- src/views/cartographie/Professionel.vue
+++ src/views/cartographie/Professionel.vue
@@ -1,6 +1,32 @@
 <template>
     <div class="contact-page">
-        <h1>Professionel page</h1>
+        <h1 class="text-5xl text-purple font-bold text-center mb-4">Professionel page</h1>
         <HelloWorld msg="C'est la Contact page" />
     </div>
+
+
+    <div id="map" class="h-[600px] w-1/2 mx-auto"></div>
 </template>
+
+
+<script>
+
+import L from 'leaflet';
+
+export default {
+    mounted() {
+        var map = L.map('map').setView([49.183333, -0.35], 13);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 15,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+    }
+};
+</script>
+
+
+
+
+<style scoped>
+
+</style>
