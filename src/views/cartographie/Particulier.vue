60b1cc61ee697f4e971a298a34fd7186a543ca26
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-19 01:54:21 +0200

    [ADD] Market leaflet

--- src/views/cartographie/Particulier.vue
+++ src/views/cartographie/Particulier.vue
@@ -12,14 +12,20 @@
 <script>
 
 import L from 'leaflet';
+import "leaflet/dist/leaflet.css";
 
 export default {
     mounted() {
         var map = L.map('map').setView([49.183333, -0.35], 13);
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 15,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var marker = L.marker([49.183333, -0.35]).addTo(map);
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+
     }
 };
 </script>
