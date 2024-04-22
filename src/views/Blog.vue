2a65b4b030b08f6a8d9528f6ce953d415f6c6114
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-22 22:36:47 +0200

    [ADD] Posts page

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -42,8 +42,53 @@
 </div>
 
 <!-- Dernier POST -->
-<div class="bg-alveoles h-[1000px] z-0 bg-cover relative mx-4">
+<div class="bg-alveoles h-[1200px] z-0 bg-cover relative mx-4 mt-20">
+  <h1 class="flex text-2xl text-purple-fonce font-bold mb-10 text justify-center">DERNIER POST</h1>
 
+        <div class="programme flex justify-center items-center max-w-[1000px]  gap-10 md:flex-col mx-auto bg-white p-4 shadow-xl rounded-3xl">
+        <div class="image-programme w-1/2">
+          <img src="../assets/landing.jpg" alt="programme" class="w-full h-[350px] object-cover rounded-3xl "/>
+        </div>
+        <div class="content-programme w-1/2 flex flex-col gap-6">
+          <h3 class="text-xl font-bold">Low Cost Latest Invented Interior Designing Ideas</h3>
+          <p class="text-md">Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpis dignissim maximus.posuere in.Contrary to popular belief.
+          Lorem Ipsum is not simply random text. It has roots in a piece of classica.</p>
+          <div class="bottom-content flex justify-normal items-center justify-between">
+          <p class="text-grey">26 Décembre 2022</p>
+          <h1 class="arrow text-xl text-grey inline-block font-bold bg-light-beige p-2 px-3 rounded-full">></h1>
+        </div>
+      </div>
+    </div>
+
+
+  <!-- LES ARTICLES & NEWS POSTS -->
+  <h1 class="flex text-2xl text-purple-fonce font-bold mb-10 text justify-center mt-20">LES ARTICLES & NEWS</h1>
+  <div class="cards flex gap-12 grow justify-center ">
+    <div class="card w-[300px] h-[375px] object-cover rounded-[25px] overflow-hidden p-4 shadow-xl border border-solid border-slate-300 relative bg-white">
+    <img src="../assets/stop.jpg" alt="stop" class="rounded-t-[25px]"/>
+    <p class="text-md -mt-12 ml-4 category-bg inline-block rounded-t-md rounded-br-md p-1">Catégorie</p>
+    <p class="text-2xl font-bold -mt-4">Let's get solution for building construction work</p>
+    <p class="text text-grey absolute bottom-4">26 Décembre 2022</p>
+    <h1 class="arrow text-xl text-grey absolute bottom-3 right-4 font-bold bg-light-beige p-2 px-3 rounded-full">></h1>
+    </div>
+
+    <div class="card w-[300px] h-[375px] object-cover rounded-[25px] overflow-hidden p-4 shadow-xl border border-solid border-slate-300 relative bg-white">
+    <img src="../assets/stop.jpg" alt="stop" class="rounded-t-[25px]"/>
+    <p class="text-md -mt-12 ml-4 category-bg inline-block rounded-t-md rounded-br-md p-1">Catégorie</p>
+    <p class="text-2xl font-bold -mt-4">Let's get solution for building construction work</p>
+    <p class="text text-grey absolute bottom-4">26 Décembre 2022</p>
+    <h1 class="arrow text-xl text-grey absolute bottom-3 right-4 font-bold bg-light-beige p-2 px-3 rounded-full">></h1>
+    </div>
+
+    <div class="card w-[300px] h-[375px] object-cover rounded-[25px] overflow-hidden p-4 shadow-xl border border-solid border-slate-300 relative bg-white">
+    <img src="../assets/stop.jpg" alt="stop" class="rounded-t-[25px]"/>
+    <p class="text-md -mt-12 ml-4 category-bg inline-block rounded-t-md rounded-br-md p-1">Catégorie</p>
+    <p class="text-2xl font-bold -mt-4">Let's get solution for building construction work</p>
+    <p class="text text-grey absolute bottom-4">26 Décembre 2022</p>
+    <h1 class="arrow text-xl text-grey absolute bottom-3 right-4 font-bold bg-light-beige p-2 px-3 rounded-full">></h1>
+    </div>
+  </div>
+  
 </div>
     <Footer />
 </template>
