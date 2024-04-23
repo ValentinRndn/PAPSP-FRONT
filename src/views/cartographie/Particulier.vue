9d7a37bc4dcb7075515d0fe99651eca1473553a1
Valentin <valentin.renaudin@my-digital-school.org>
2024-04-23 18:30:41 +0200

    [ADD] Map page particulier

--- src/views/cartographie/Particulier.vue
+++ src/views/cartographie/Particulier.vue
@@ -1,48 +1,107 @@
 <template>
-    <NavigationBar />
-    <div class="contact-page">
-        <h1 class="text-5xl text-purple font-bold text-center mb-4">Particulier page</h1>
-        <HelloWorld msg="C'est la Contact page" />
+  <NavigationBar />
+
+  <div class="page-container flex h-[700px] items-center">
+    <div class="categories-container bg-purple-fonce w-[1/3] h-full px-6 flex flex-col justify-center">
+      <h1 class="text-white text-2xl font-bold mb-6 font-cgothic">PAPSP VOUS ORIENTE :</h1>
+      <div class="checkboxes flex flex-col gap-3 text-white text-xl">
+
+        <div class="checkboxes flex flex-col gap-4 font-poppins">
+          <div class="checkbox flex gap-3 color-red">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Trouver des préservatifs ou du lubrifiant</label>
+          </div>
+
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Me faire dépister</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Accéder à un traitement
+d’urgence</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Accéder à la PReP</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Voir un médecin</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Interrompre une grossesse</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Trouver du matériel de
+drogue à moindre 
+risque</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Trouver un soutien
+communautaire</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Porter plainte</label>
+        </div>
+
+        
+          <div class="checkbox flex gap-3">
+            <input type="checkbox" id="scales" name="scales" />
+            <label for="scales">Parler à quelqu’un après
+une agression</label>
+        </div>
     </div>
-
-
-    <div id="map" class="h-[600px] w-1/2 mx-auto"></div>
-
-    <Footer />
+        </div>
+    </div>
+    <div id="map" class="h-full  w-full"></div>
+  </div>
+  <Footer />
 </template>
 
-
 <script>
-
-
-import L from 'leaflet';
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import NavigationBar from "../../components/NavigationBar.vue";
-import Footer from "../../components/Footer.vue"
+import Footer from "../../components/Footer.vue";
 
 export default {
-    mounted() {
-        var map = L.map('map').setView([49.183333, -0.35], 13);
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+  mounted() {
+    var map = L.map("map").setView([49.183333, -0.35], 13);
+    L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
 
-var marker = L.marker([49.183333, -0.35]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
-
-
-    },
-    components: {
-      Footer, 
-      NavigationBar,
-    }
+    var marker = L.marker([49.183333, -0.35]).addTo(map);
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+  },
+  components: {
+    Footer,
+    NavigationBar,
+  },
 };
 </script>
 
-
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
