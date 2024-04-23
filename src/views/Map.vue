e738a72de07a1fd314364c4dc8908d3275bfe2b8
Valentin <valentin.renaudin@my-digital-school.org>
2024-04-23 18:02:41 +0200

    [Fix] Background map

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,7 @@
 <template>
     <NavigationBar />
 
-    <div class="bg-back-carto flex w-[100vw] h-[100vh]">
+    <div class="bg-back-carto flex h-[80vh] bg-cover bg-no-repeat">
         <div class="w-[50vw] flex justify-center items-center">
             <div class="flex flex-col w-[400px] h-[200px] justify-center font-cgothic   ">
                 <router-link to="/cartographie/Particulier" class="bg-purple-fonce text-white px-4 py-6 w-full rounded-md mt-6 text-md font-bold text-center hover:scale-105 duration-300">JE CHERCHE UN SERVICE</router-link>
