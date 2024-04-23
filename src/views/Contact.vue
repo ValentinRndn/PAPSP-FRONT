5189edd6ee99dd0ea6708358e69c169bc7912da0
Valentin <valentin.renaudin@my-digital-school.org>
2024-04-23 17:33:28 +0200

    [FIX] Font & CSS contact

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -10,57 +10,63 @@
 
 <!-- Formulaire de contact -->
 <div class="contact-container flex flex-wrap justify-center items-center gap-10 mt-20 font-jost">
-    <div class="info-contact font-jost-sans flex flex-col bg-purple-fonce p-8 rounded-xl text-white gap-4">
-        <div class="mail-container flex justify-center items-center gap-2 ">
+    <div class="info-contact font-jost-sans flex flex-col bg-purple-fonce p-10 rounded-xl text-white gap-4">
+        <div class="mail-container flex items-center gap-2">
             <img src="../assets/mail_icon.png" alt="mail-icon" class="size-10">
             <p class="mail">info@youdomain.com</p>
         </div>
-        <div class="phone-container flex justify-center items-center gap-2">
-            <img src="../assets/phone_icon.png" alt="mail-icon" class="size-10">
+        <div class="phone-container flex items-center gap-2">
+            <img src="../assets/phone_icon.png" alt="phone-icon" class="size-10">
             <p class="phone">+33 65 77 45 65</p>
         </div>
     </div>
-<div class="contact-form bg-white flex flex-col justify-center items-center gap-6 mt-6 ">
-  <form class="flex flex-col gap-6">
-    <div class="first-row flex md:flex-row md:gap-8 w-full gap-8">
-      <input type="text" placeholder="Nom" class="border-b border-purple-fonce text-md w-full md:w-auto pb-4">
-      <input type="email" placeholder="Email" class="border-b border-purple-fonce text-md w-full md:w-auto pb-4">
+    <div class="contact-form bg-white flex flex-col justify-center items-center gap-6 mt-6 ">
+        <form class="flex flex-col gap-6">
+            <div class="first-row flex md:flex-row md:gap-8 w-full gap-8">
+                <input type="text" placeholder="Nom" class="border-b border-purple-fonce text-xl w-full md:w-auto pb-4">
+                <input type="email" placeholder="Email" class="border-b border-purple-fonce text-xl w-full md:w-auto pb-4">
+            </div>
+            <div class="second-row flex md:flex-row md:gap-8 w-full gap-8">
+                <input type="text" placeholder="Objet" class="border-b border-purple-fonce text-xl w-full md:w-auto pb-4">
+                <input type="tel" placeholder="Téléphone" class="border-b border-purple-fonce text-xl w-full md:w-auto pb-4">
+            </div>
+            <div class="textarea w-full">
+                <textarea placeholder="Votre message" class="border-b border-purple-fonce text-xl w-full"></textarea>
+            </div>
+            <div class="submit flex justify-end w-full">
+                <button class="bg-purple-fonce text-white font-jost-sans font-semibold text-xl py-3 rounded-md px-6 transition-transform duration-300 hover:scale-105">Envoyer</button>
+            </div>
+        </form>   
     </div>
-    <div class="second-row flex md:flex-row md:gap-8 w-full gap-8">
-    <input type="text" placeholder="Objet" class="border-b border-purple-fonce text-md w-full md:w-auto pb-4">
-      <input type="telephone" placeholder="Téléphone" class="border-b border-purple-fonce text-md w-full md:w-auto pb-4">
-    </div>
-    <div class="textarea w-full">
-      <textarea placeholder="Votre message" class="border-b border-purple-fonce text-md w-full"></textarea>
-    </div>
-    <div class="submit flex justify-end w-full">
-      <button class="bg-purple-fonce text-white font-jost-sans font-semibold text-md py-3 rounded-md px-6 transition-transform duration-300 hover:scale-105">Envoyer</button>
-    </div>
-  </form>   
 </div>
-</div>
+
 
 
 <!-- Réseaux sociaux -->
-<div class="bg-alveoles h-[700px] z-0 bg-cover relative mx-4 sm:w-full sm:h-full">
-    <div class="text-socials relative mt-24">
-        <h1 class="socials-medias text-center text-5xl font-bold text-contact-grey font-raleway">Réseaux sociaux</h1>
-        <p class="text-center text-light-grey font-raleway">Vous pouvez également nous contacter sur chacun de nos réseaux sociaux. Nous vous répondrons dans les plus brefs délais..</p>
-    </div>
-    <div class="socials relative flex justify-center items-center mt-10 z-20 w-full gap-6">
-        <a href="https://www.facebook.com" target="_blank" class="social bg-purple-fonce w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
-            <img src="../assets/contact/socials/facebook.png" alt="facebook" class="w-[50px] h-[50px]">
-        </a>
-        <a href="https://www.instagram.com" target="_blank" class="social bg-purple-fonce w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
-            <img src="../assets/contact/socials/insta.png" alt="instagram" class="w-[50px] h-[50px]">
-        </a>
-        <a href="https://www.linkedin.com" target="_blank" class="social bg-purple-fonce  w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
-            <img src="../assets/contact/socials/linkedinn.png" alt="linkedin" class="w-[50px] h-[50px]">
-        </a>
-        <a href="https://www.tiktok.com" target="_blank" class="social bg-purple-fonce  w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
-            <img src="../assets/contact/socials/tiktokk.png" alt="tiktok" class="w-[50px] h-[50px]">
-        </a>
+<div class="bg-alveoles h-[700px] z-0 bg-cover relative sm:w-full sm:h-full">
+    <div class="mx-28"> <!-- Ajout de la classe mx-4 ici -->
+        <div class="text-socials relative mt-24">
+            <h1 class="socials-medias text-center text-5xl font-bold text-contact-grey font-raleway">Réseaux sociaux</h1>
+            <p class="text-center text-light-grey font-raleway">Vous pouvez également nous contacter sur chacun de nos réseaux sociaux. Nous vous répondrons dans les plus brefs délais..</p>
+        </div>
+        <div class="socials relative flex justify-center items-center mt-10 z-20 w-full gap-6">
+            <div class="social-wrapper flex justify-center w-full gap-6"> <!-- Div wrapper ajoutée ici -->
+                <a href="https://www.facebook.com" target="_blank" class="social bg-purple-fonce w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
+                    <img src="../assets/contact/socials/facebook.png" alt="facebook" class="w-[50px] h-[50px]">
+                </a>
+                <a href="https://www.instagram.com" target="_blank" class="social bg-purple-fonce w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
+                    <img src="../assets/contact/socials/insta.png" alt="instagram" class="w-[50px] h-[50px]">
+                </a>
+                <a href="https://www.linkedin.com" target="_blank" class="social bg-purple-fonce  w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
+                    <img src="../assets/contact/socials/linkedinn.png" alt="linkedin" class="w-[50px] h-[50px]">
+                </a>
+                <a href="https://www.tiktok.com" target="_blank" class="social bg-purple-fonce  w-1/4 flex justify-center items-center py-5 rounded-md transition-transform duration-300 hover:scale-105">
+                    <img src="../assets/contact/socials/tiktokk.png" alt="tiktok" class="w-[50px] h-[50px]">
+                </a>
+            </div>
+        </div>
     </div>  
+
 
 
 
@@ -97,7 +103,7 @@ export default {
 }).addTo(map);
 
 var marker = L.marker([49.183333, -0.35]).addTo(map);
-marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+marker.bindPopup("<b>EPSM CAEN</b><br>15 ter Rue Saint-Ouen").openPopup();
 
 
     }
