53aaaf1a444ba9193a76a3309f593ec943c31699
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-04-29 22:05:34 +0200

    [EDIT] Menu burger

--- src/components/NavigationBarMobile.vue
+++ src/components/NavigationBarMobile.vue
@@ -1,47 +1,61 @@
 <template>
-  <aside>
-    <div class="icon-burger flex w-full items-center justify-evenly">
-        <img src="../assets/logo_typo.png" alt="logo" class="object-cover w-[300px]">
-        <img src="../assets/icon-burger.png" @click="toggleMenu()" alt="burger" class="object-cover w-[35px] h-[35px]">
-
-    </div>
-    <ul class="navbar flex flex-col gap-10 text-xl mt-10 font-cgothic font-bol justify-center items-center bg-purple text-white opacity-0">
-      <li class="menu">
-        <router-link to="/" :class="{ 'text-purple underline ': currentPage === '/' }">ACCUEIL</router-link>
-      </li>      
-      <li class="menu">
-        <router-link to="/map" :class="{'text-purple underline ':currentPage === '/map' }">CARTOGRAPHIE</router-link>
-      </li>
-      <li class="menu">
-        <router-link to="/documentation" :class="{ 'text-purple underline ': currentPage === '/documentation' }">DOCUMENTATION</router-link>
-      </li>
-      <li class="menu">
-        <router-link to="/contact" :class="{ 'text-purple underline ': currentPage === '/contact' }">CONTACT</router-link>
-      </li>
-      <li class="menu">
-        <router-link to="/blog" :class="{ 'text-purple underline ': currentPage === '/contact' }">BLOG</router-link>
-      </li>
-      <li class="menu">
-        <router-link to="/APropos" :class="{ 'text-purple underline ':currentPage === '/APropos' }">EN SAVOIR +</router-link>
-      </li>
-    </ul>
-  </aside>
-</template>
-
-<script>
-export default {
+    <aside>
+      <div class="icon-burger flex w-full items-center justify-evenly z-30">
+        <img v-if="!menuOpen" src="../assets/logo_typo.png" alt="logo" class="object-cover w-[300px]">
+        <img v-else src="../assets/logo_typo_blanc.png" alt="close icon" @click="toggleMenu" class="object-cover w-[300px]">
+    
+        <h1 @click="toggleMenu" id="icon" class="text-3xl font-bold cursor-pointer">X</h1>
+      </div>
+      <ul class="navbar w-full flex flex-col gap-10 text-xl font-cgothic font-semibold justify-center items-center bg-purple fixed text-white py-5 opacity-0 transition-opacity duration-300">
+        <li class="menu">
+          <router-link to="/" :class="{ 'text-purple underline': currentPage === '/' }">ACCUEIL</router-link>
+        </li>      
+        <li class="menu">
+          <router-link to="/map" :class="{ 'text-purple underline': currentPage === '/map' }">CARTOGRAPHIE</router-link>
+        </li>
+        <li class="menu">
+          <router-link to="/documentation" :class="{ 'text-purple underline': currentPage === '/documentation' }">DOCUMENTATION</router-link>
+        </li>
+        <li class="menu">
+          <router-link to="/contact" :class="{ 'text-purple underline': currentPage === '/contact' }">CONTACT</router-link>
+        </li>
+        <li class="menu">
+          <router-link to="/blog" :class="{ 'text-purple underline': currentPage === '/blog' }">BLOG</router-link>
+        </li>
+        <li class="menu">
+          <router-link to="/APropos" :class="{ 'text-purple underline': currentPage === '/APropos' }">EN SAVOIR +</router-link>
+        </li>
+      </ul>
+    </aside>
+  </template>
+  
+  <script>
+  export default {
     name: 'NavigationBarMobile',
-
+    data() {
+    return {
+      menuOpen: false
+    };
+    },
     methods: {
-        toggleMenu() {
-            const menu = document.querySelector('.navbar');
-            menu.classList.toggle('opacity-0');
-        }
+      toggleMenu() {
+        const menu = document.querySelector('.navbar');
+        const iconDiv = document.querySelector('.icon-burger');
+        menu.classList.toggle('opacity-0');
+        menu.classList.toggle('z-20');
+        iconDiv.classList.toggle('bg-purple');
+        iconDiv.classList.toggle('transition-opcacity');
+        iconDiv.classList.toggle('duration-300');
+        iconDiv.classList.toggle('text-white');
+        this.menuOpen = !this.menuOpen;
+
+
+      }
     }
-}
-
-</script>
-
-<style>
-
-</style>
+  }
+  </script>
+  
+  <style>
+  /* Ajoutez ici vos styles personnalisés si nécessaire */
+  </style>
+  
