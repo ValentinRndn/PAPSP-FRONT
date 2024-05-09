b0d0f0b13cbb5f24cd1b4d0b561b1daaa3483e12
Valentin renaudin <valentin.renaudin@my-digital-school.org>
2024-05-09 14:41:58 +0200

    [ADD] Component Guide Content

--- src/components/Guide/GuideContent.vue
+++ src/components/Guide/GuideContent.vue
@@ -4,8 +4,55 @@
      <router-link  class="button-prostitution border border-solid border-light-grey py-2 px-6 text-lg font-cgothic" to="/o">PROSTITUTION </router-link>
      <router-link  class="button-prostitution border border-solid border-light-grey py-2 px-6 text-lg font-cgothic" to="/salut">PROSITUTION MINEUR</router-link>
 
+    </div>
 
+    <div class="documents-container mt-14 mx-10 flex gap-5 flex-wrap">
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
 
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        
+        <div class="doc-container w-[300px] flex flex-col items-center justify-center text-center gap-3">
+            <img src="../../assets/guide/imgtest.png" class="w-full" alt="test">
+            <h3 class="w-full font-semibold">PROSITUTION DES MINEURS : REPERER, PROTEGER & ORIENTER</h3>
+            <p class="w-full">Centre de victomologie pour mineurs</p>
+        </div>
+
+        
     </div>
 
 
